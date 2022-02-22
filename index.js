42eba82e0f5080aaf52ff8c3b1515349f4f5e30b
vinita31 <85412457+vinita31@users.noreply.github.com>
2022-02-22 11:45:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// initializing all the varibales
+var input = document.querySelector('#input');
+var result = document.querySelector('#result');
+var button = document.querySelectorAll('button');
+var clear = document.querySelector('#clear');
+var equal = document.querySelector('#equal');
+
+//declaring a array which collect input from user as a form of string
+var a = [];
+
+// now we apply different condition
+// user press ac button
+
+clear.addEventListener("click", () =>
+{
+    a = [''];
+    result.innerHTML = 0;
+    input.className = 'input';
+    result.className = 'result';
+})
+
+// if user press any button expect clear button then we have to display that number on input screen and calculate the answer
+button.forEach((btn) =>
+{
+    btn.addEventListener("click", () =>
+    {
+        // when clicked button is not a clear button
+        if (!btn.id.match('clear')) {
+            //push the input press by user
+            a.push(btn.value);
+            // join the string 
+            input.innerHTML = a.join('');
+            //to evaluate  answer
+            if (btn.classList.contains('num-btn')) {
+                result.innerHTML = eval(a.join(''));
+            }
+        }
+        if (btn.id.match('clear')) {
+            a.pop();
+            input.innerHTML = a.join('');
+            result.innerHTML = eval(a.join(''));
+        }
+        if (btn.id.match('equal')) {
+            input.className = 'result';
+            result.className = 'input';
+        }
+    })
+})
